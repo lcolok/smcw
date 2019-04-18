24f2e8932276ff88e9c5e48813665b3d191ab079
lcolok <425311101@qq.com>
2019-04-19 00:39:34 +0800

    采用raw-loader的方式进行LeanCloud的初始化

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
@@ -11,15 +10,13 @@ Vue.config.productionTip = false
 
 import AV from 'leancloud-storage'
 
-//以下这个为美国节点
-/* AV.init({
-  appId: 'eiFnyWWSdKM4YOW6jdrc77AP-MdYXbMMI',
-  appKey: 'JWmRjxc5E33n2lebvMDhURsH',
-}); */
+import current_app_id from '!raw-loader!../.leancloud/current_app_id';
+import current_app_key from '!raw-loader!../.leancloud/current_app_key';
+
 AV.init({
-  appId: 'OoFIwrWMmpITh2amlEu1ydM4-gzGzoHsz',
-  appKey: 'iwJMvV3U3JqKMkSmdLFJU1v4',
-});
+  appId: current_app_id,
+  appKey: current_app_key,
+})
 
 Vue.prototype.$AV = AV;
 
