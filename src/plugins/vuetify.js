47495ffcf4c982110725e1de8313504e72f842ce
lcolok <425311101@qq.com>
2019-04-20 08:57:31 +0800

    不使用md图标

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,8 +4,11 @@ import 'vuetify/src/stylus/app.styl'
 
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify, {
-  iconfont: ['mdi','md']
+  iconfont: [
+    'mdi',
+    // 'md'
+  ]
 })
