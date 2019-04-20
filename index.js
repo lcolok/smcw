e2a2021e0e9476cea7c3fd659ac06ccf2d811dbd
lcolok <425311101@qq.com>
2019-04-21 01:18:07 +0800

    尝试加入now.sh，但似乎失败了

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var AV = require('leanengine');
+try{
+  AV.init({
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  });
+}catch(e){
+  
+}
 
-AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-});
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
