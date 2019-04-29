7e2d819dfb1f77a58e9d4c9375930212577664ae
lcolok(JUB) <425311101@qq.com>
2019-04-29 18:47:35 +0800

    可以拖动后识别目标事件

--- src/vue/Draggable.vue
+++ src/vue/Draggable.vue
@@ -10,23 +10,30 @@
       <a-card title="Card Title">
         <draggable
           class="list-group"
-          tag="ul"
           v-model="list"
           v-bind="dragOptions"
+          :move="checkMove"
           @start="drag = true"
           @end="drag = false"
         >
           <!-- <transition-group type="transition" :name="!drag ? 'flip-list' : null"> -->
-            <a-card-grid  style="width:33%;textAlign:'center';height: 33vw;" class="list-group-item" v-for="element in list" :key="element.order">
-              <i
-                :class="
+          <a-card-grid
+            style="width:33%;textAlign:'center';height: 33vw;"
+            class="list-group-item"
+            v-for="element in list"
+            :key="element.order"
+            @dragend="dragend"
+            @dragenter="dragenter"
+          >
+            <i
+              :class="
                 element.fixed ? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'
               "
-                @click="element.fixed = !element.fixed"
-                aria-hidden="true"
-              ></i>
-              {{ element.name }}
-            </a-card-grid>
+              @click="element.fixed = !element.fixed"
+              aria-hidden="true"
+            ></i>
+            {{ element.name }}
+          </a-card-grid>
           <!-- </transition-group> -->
         </draggable>
       </a-card>
@@ -46,12 +53,12 @@ const message = [
   "vue.js 2.0",
   "based",
   "on",
-  "Sortablejs"
+  "Sortablejs",
+  "apple"
 ];
 export default {
   name: "transition-example-2",
   display: "Transitions",
-  order: 7,
   components: {
     draggable
   },
@@ -64,6 +71,17 @@ export default {
     };
   },
   methods: {
+    dragend: function(evt) {
+      // console.log("dragend",evt);
+    },
+    dragenter: evt => {
+      console.log("dragend", evt);
+      console.log(evt.srcElement);
+    },
+    checkMove: function(evt) {
+      // console.log(evt);
+      return evt.draggedContext.element.name !== "apple";
+    },
     sort() {
       this.list = this.list.sort((a, b) => a.order - b.order);
     }
@@ -71,10 +89,17 @@ export default {
   computed: {
     dragOptions() {
       return {
-        animation: 200,
-        group: "description",
+        touchStartThreshold: 0, // px, how many pixels the point should move before cancelling a delayed drag event
+        sort: false,
+        group: "dashboard",
         disabled: false,
-        ghostClass: "ghost"
+        // ghostClass: "custom_sortable_ghost", // Class name for the drop placeholder
+        // chosenClass: "custom_sortable_chosen", // Class name for the chosen item
+        dragClass: "custom_sortable_drag", // Class name for the dragging item
+        animation: 200, // ms, animation speed moving items when sorting, `0` — without animation
+        // easing: "cubic-bezier(1, 0, 0, 1)" // Easing for animation. Defaults to null. See https://easings.net/ for examples.
+        draggable: ".list-group-item", // Specifies which items inside the element should be draggable
+        dragoverBubble: true
       };
     }
   }
@@ -91,9 +116,17 @@ export default {
 .no-move {
   transition: transform 0s;
 }
-.ghost {
-  opacity: 0.5;
+.custom_sortable_ghost {
+  opacity: 0.8;
   background: #c8ebfb;
+}
+.custom_sortable_chosen {
+  opacity: 0.8;
+  background: #fbc8c8;
+}
+.custom_sortable_drag {
+  opacity: 0.8;
+  background: #cefbc8;
 }
 .list-group {
   min-height: 20px;
