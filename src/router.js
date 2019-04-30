3b1ef0cdb5515af21ad90b57de7d9ca825230836
lcolok(JUB) <425311101@qq.com>
2019-04-30 17:04:02 +0800

    按照文件夹排布进行页面的加载

--- src/router.js
+++ src/router.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './vue/Home.vue'
-import NotFound from './vue/NotFound.vue'
 
-import Upload from './vue/Upload.vue'
-import Login from './vue/Login.vue'
-import SignUp from './vue/SignUp.vue'
-import Sortable from './vue/Sortable.vue'
 
 Vue.use(Router);
 
@@ -23,97 +17,135 @@ AV.init({
 Vue.prototype.$AV = AV;
 
 
-const router = new Router({
-  // mode: 'history',
-  routes: [
-    {
-      path: '/map',
-      name: '路由表导航',
-      component: () => import('./vue/Map.vue'),
-      meta: {
-        title: '路由表导航',
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login,
-      meta: {
-        title: '登录',
-      }
-    },
-    {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUp,
-      meta: {
-        title: '注册',
-      }
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta: {
-        title: '首页入口',
-        // requiresAuth: true,
-      }
-    },
-    {
-      path: '/u',
-      name: 'upload',
-      component: Upload,
-      meta: {
-        title: '七牛上传',
-        requiresAuth: true,
-      }
-    },
-    {
-      path: '/sortable',
-      name: 'sortable',
-      component: Sortable,
-      meta: {
-        title: 'Sortable',
-        // requiresAuth: true,
-      }
-    },
-    {
-      path: '/mescroll',
-      name: 'Mescroll',
-      meta: {
-        title: 'mescroll测试页'
-      },
-      component: () => {
-        import('./vue/Mescroll.vue');
+// const router = new Router({
+//   // mode: 'history',
+//   routes: [
+//     {
+//       path: '/map',
+//       name: '路由表导航',
+//       component: () => import('./vue/Map.vue'),
+//       meta: {
+//         title: '路由表导航',
+//       }
+//     },
+//     {
+//       path: '/login',
+//       name: 'login',
+//       component: Login,
+//       meta: {
+//         title: '登录',
+//       }
+//     },
+//     {
+//       path: '/signup',
+//       name: 'SignUp',
+//       component: SignUp,
+//       meta: {
+//         title: '注册',
+//       }
+//     },
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home,
+//       meta: {
+//         title: '首页入口',
+//         // requiresAuth: true,
+//       }
+//     },
+//     {
+//       path: '/u',
+//       name: 'upload',
+//       component: Upload,
+//       meta: {
+//         title: '七牛上传',
+//         requiresAuth: true,
+//       }
+//     },
+//     {
+//       path: '/sortable',
+//       name: 'sortable',
+//       component: Sortable,
+//       meta: {
+//         title: 'Sortable',
+//         // requiresAuth: true,
+//       }
+//     },
+//     {
+//       path: '/mescroll',
+//       name: 'Mescroll',
+//       meta: {
+//         title: 'mescroll测试页'
+//       },
+//       component: () => {
+//         import('./vue/Mescroll.vue');
 
-      }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      //路线级别代码分隔
-      // this generates a separate chunk (about.[hash].js) for this route
-      //这将为该路由生成一个单独的块(大约.[hash].js)
-      // which is lazy-loaded when the route is visited.
-      //当访问路由时，它会进行懒加载加载。
-      component: () => import(/* webpackChunkName: "about" */ './vue/About.vue')
-    },
-    {
-      /* Not Found 路由，必须是最后一个路由 */
-      path: '*',
-      name: 'NotFound',
-      component: NotFound,
-      meta: {
-        title: '404 Not Found'
-      }
-    },
-  ]
+//       }
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       //路线级别代码分隔
+//       // this generates a separate chunk (about.[hash].js) for this route
+//       //这将为该路由生成一个单独的块(大约.[hash].js)
+//       // which is lazy-loaded when the route is visited.
+//       //当访问路由时，它会进行懒加载加载。
+//       component: () => import(/* webpackChunkName: "about" */ './vue/About.vue')
+//     },
+//     {
+//       /* Not Found 路由，必须是最后一个路由 */
+//       path: '*',
+//       name: 'NotFound',
+//       component: NotFound,
+//       meta: {
+//         title: '404 Not Found'
+//       }
+//     },
+//   ]
+
+// })
+
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('./vue/Home.vue'),
+    meta: {
+      title: '首页入口',
+      // requiresAuth: true,
+    }
+  },
+];
+
+importPages(require.context('./vue', false, /\.vue$/, 'lazy'))
+function importPages(r) {
+
+  r.keys().forEach(key => {
+    console.log(r(key));
+    routes.push({
+      path: (key.toLowerCase().split('.'))[1],
+      component: () => r(key)
+    })
+  });
+}
+
+routes.push({
+  /* Not Found 路由，必须是最后一个路由 */
+  path: '*',
+  name: 'NotFound',
+  component: () => import('./vue/NotFound.vue'),
+  meta: {
+    title: '404 Not Found',
+    // hideOnTheMap: true
+  }
 })
 
-
-
-
+const router = new Router({
+  routes: routes
+})
 
 router.beforeEach((to, from, next) => {//to即将进入的目标路由对象，from当前导航正要离开的路由， next  :  下一步执行的函数钩子
   document.title = to.meta.title;//给出指定的标题名称
