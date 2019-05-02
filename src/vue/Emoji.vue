8c20c1b2d76334b4a7bacd1c5812ab66a09519a1
lcolok <425311101@qq.com>
2019-05-03 03:40:32 +0800

    加了个emoji组件

---
+++ src/vue/Emoji.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <VEmojiPicker :pack="pack" @select="selectEmoji" />
+  </div>
+</template>
+
+<script>
+import VEmojiPicker from 'v-emoji-picker';
+import packData from 'v-emoji-picker/data/emojis.json';
+
+export default {
+  name: 'Demo',
+  components: {
+    VEmojiPicker
+  },
+  data: () => ({
+    pack: packData
+  }),
+  methods: {
+    selectEmoji(emoji) {
+      console.log(emoji)
+    }
+  }
+}
+</script>
