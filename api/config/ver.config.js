4561c1940a7734fe59110df2db0209ee6a1d95af
lcolok(Home) <425311101@qq.com>
2019-05-05 00:30:10 +0800

    整理router.js

--- api/config/ver.config.js
+++ api/config/ver.config.js
@@ -1,9 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 0,
-		build: 5
+		minor: 1,
+		build: 7,
+		revision: 0
 	},
-	whole: "0.0.5(103)",
-	commits: 103
+	whole: "0.1.7(104)",
+	commits: 104
 }
--- src/router.js
+++ src/router.js
@@ -11,108 +11,7 @@ if (process.env.NODE_ENV == "development") {//如果是处于开发状态的话
   AV._setServerURLs('http://localhost:' + custom_dev_port)//设置本地服务器端口(必须先进行 lean up 操作)
 }
 
-// const router = new Router({
-//   // mode: 'history',
-//   routes: [
-//     {
-//       path: '/map',
-//       name: '路由表导航',
-//       component: () => import('./vue/Map.vue'),
-//       meta: {
-//         title: '路由表导航',
-//       }
-//     },
-//     {
-//       path: '/login',
-//       name: 'login',
-//       component: Login,
-//       meta: {
-//         title: '登录',
-//       }
-//     },
-//     {
-//       path: '/signup',
-//       name: 'SignUp',
-//       component: SignUp,
-//       meta: {
-//         title: '注册',
-//       }
-//     },
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: Home,
-//       meta: {
-//         title: '首页入口',
-//         // requiresAuth: true,
-//       }
-//     },
-//     {
-//       path: '/u',
-//       name: 'upload',
-//       component: Upload,
-//       meta: {
-//         title: '七牛上传',
-//         requiresAuth: true,
-//       }
-//     },
-//     {
-//       path: '/sortable',
-//       name: 'sortable',
-//       component: Sortable,
-//       meta: {
-//         title: 'Sortable',
-//         // requiresAuth: true,
-//       }
-//     },
-//     {
-//       path: '/mescroll',
-//       name: 'Mescroll',
-//       meta: {
-//         title: 'mescroll测试页'
-//       },
-//       component: () => {
-//         import('./vue/Mescroll.vue');
-
-//       }
-//     },
-//     {
-//       path: '/about',
-//       name: 'about',
-//       // route level code-splitting
-//       //路线级别代码分隔
-//       // this generates a separate chunk (about.[hash].js) for this route
-//       //这将为该路由生成一个单独的块(大约.[hash].js)
-//       // which is lazy-loaded when the route is visited.
-//       //当访问路由时，它会进行懒加载加载。
-//       component: () => import(/* webpackChunkName: "about" */ './vue/About.vue')
-//     },
-//     {
-//       /* Not Found 路由，必须是最后一个路由 */
-//       path: '*',
-//       name: 'NotFound',
-//       component: NotFound,
-//       meta: {
-//         title: '404 Not Found'
-//       }
-//     },
-//   ]
-
-// })
-
 const routes = []
-
-// const routes = [
-//   {
-//     path: '/',
-//     name: 'home',
-//     component: () => import('./vue/Home.vue'),
-//     meta: {
-//       title: '首页入口',
-//       // requiresAuth: true,
-//     }
-//   },
-// ];
 
 importPages(require.context('./vue', false, /\.vue$/))
 function importPages(r) {
@@ -136,7 +35,7 @@ function importPages(r) {
     routes
       .sort((a, b) => {//按照自定义的优先级来排序
         // console.log(a.meta.order);
-        if ((a.meta.order||-1) < (b.meta.order||-1)) {
+        if ((a.meta.order || -1) < (b.meta.order || -1)) {
           return -1
         } else {
           return 0
@@ -157,20 +56,7 @@ function importPages(r) {
         }
       })
   });
-
-
 }
-
-// routes.push({
-//   /* Not Found 路由，必须是最后一个路由 */
-//   path: '*',
-//   name: 'NotFound',
-//   component: () => import('./vue/NotFound.vue'),
-//   meta: {
-//     title: '404 Not Found',
-//     // hideOnTheMap: true
-//   }
-// })
 
 const router = new Router({
   routes: routes
@@ -178,8 +64,6 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {//to即将进入的目标路由对象，from当前导航正要离开的路由， next  :  下一步执行的函数钩子
   document.title = to.meta.title;//给出指定的标题名称
-
-
 
   if (to.path === '/login') { next() }  // 如果即将进入登录路由，则直接放行
 
@@ -202,6 +86,5 @@ router.beforeEach((to, from, next) => {//to即将进入的目标路由对象，f
   }  //如果不需要登录验证，或者已经登录成功，则直接放行
 
 })
-
 
 export default router
