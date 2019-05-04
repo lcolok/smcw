6d14a64185a3e72e60cb3024f5b2c6a14fcbafd1
lcolok(Home) <425311101@qq.com>
2019-05-04 21:03:14 +0800

    把gulp的相关依赖包全都放进开发依赖里

--- api/config/ver.config.js
+++ api/config/ver.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	parts: {
 		major: 0,
 		minor: 0,
-		revision: 5,
-		build: 33
+		revision: 6,
+		build: 2
 	},
-	whole: "0.0.5(33)",
-	commits: 100
+	whole: "0.0.6(2)",
+	commits: 101
 }
