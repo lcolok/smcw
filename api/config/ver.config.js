d8ec4d3e733846897dd4e43408f568c6fd662e0e
lcolok(Home) <425311101@qq.com>
2019-05-06 03:39:49 +0800

    实现端口的躲避（自动+1寻找可用端口）

--- api/config/ver.config.js
+++ api/config/ver.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	parts: {
 		major: 0,
-		minor: 11,
-		build: 19,
+		minor: 12,
+		build: 6,
 		revision: 0
 	},
-	whole: "0.11.19(124)",
-	commits: 124
+	whole: "0.12.6(125)",
+	commits: 125
 }
